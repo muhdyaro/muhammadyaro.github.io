ff5f59dc551f8fa006696f8377aa56b230920109
Muhammad Yaro <muhdayaro@gmail.com>
2024-04-28 15:57:41 +0100

    Update

--- script.js
+++ script.js
@@ -4,7 +4,3 @@ document.getElementById("about");
 aboutElement.addEventListener("dragstart",function(event){
     event.dataTransfer.setData("text/plain",event.target.id);
 });
-
-aboutElement.addEventListener("drag", function(event){
-})c:\Users\My Pc\Downloads\raining.jpeg c:\Users\My Pc\Downloads\snow.jpeg c:\Users\My Pc\Downloads\summer.
-c:\Users\My Pc\Downloads\raining.jpeg c:\Users\My Pc\Downloads\snow.jpeg c:\Users\My Pc\Downloads\summer.jpeg
